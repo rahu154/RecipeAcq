60e809009f3a42eaa138a168855168bbb69364c5
rahulit <107352305+rahu154@users.noreply.github.com>
2025-02-07 14:22:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+<link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
